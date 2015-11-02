ebf24c2ab424cb006c0d137755e3d4d20e29da61
zengyan <zengyan03@outlook.com>
2015-11-02 09:38:28 +0800

    feature(main-43)test3

--- routes/views/about.js
+++ routes/views/about.js
@@ -9,7 +9,7 @@ exports = module.exports = function(req, res) {
 	locals.page.title = 'About SydJS';
 	
 	locals.organisers = [
-		{ name: '方糖', image: '/images/organiser-craig_sharkie.jpg', weibo: 'twalve',       title: 'Founder, MC, coordinator' },
+		{ name: '方糖', image: '/images/organiser-craig_sharkie.jpg', weibo: 'u/5286061009',       title: 'Founder, MC, coordinator' },
 		{ name: '曾䶮', image: '/images/organiser-zeng_yan.jpg',   weibo: 'allenxiaoai', title: 'Atlassian coordinator' },
 		{ name: '仇紫荷',image: '/images/organiser-gil_davidson.jpg',     weibo: 'u/2668961191',   title: 'Atlassian coordinator' },
 		{ name: '应泽峰', image: '/images/organiser-adam_ahmed.jpg',    weibo: 'u/5019398138',   title: 'Atlassian coordinator' },
