d6250a9c1c14fc1f97da940b60087ebd6797f69d
zengyan <zengyan03@outlook.com>
2015-11-02 08:24:31 +0800

    feature（main43）：test 修改名字信息

--- routes/views/about.js
+++ routes/views/about.js
@@ -9,11 +9,11 @@ exports = module.exports = function(req, res) {
 	locals.page.title = 'About SydJS';
 	
 	locals.organisers = [
-		{ name: 'Craig Sharkie', image: '/images/organiser-craig_sharkie.jpg', twitter: 'twalve',       title: 'Founder, MC, coordinator' },
-		// { name: 'Thinkmill', image: '/images/organiser-thinkmill.jpg',     twitter: 'thethinkmill', title: 'Site coordinator' },
-		{ name: 'Gil Davidson',     image: '/images/organiser-gil_davidson.jpg',     twitter: 'iamnotyourbroom',   title: 'Atlassian coordinator' },
-		{ name: 'Adam Ahmed',    image: '/images/organiser-adam_ahmed.jpg',    twitter: 'hitsthings',   title: 'Atlassian coordinator' },
-		{ name: 'Lachlan Hardy', image: '/images/organiser-lachlan_hardy.jpg', twitter: 'lachlanhardy', title: 'Community coordinator' }
+		{ name: '方糖', image: '/images/organiser-craig_sharkie.jpg', twitter: 'twalve',       title: 'Founder, MC, coordinator' },
+		{ name: '曾䶮', image: '/images/organiser-zeng_yan.jpg',     twitter: 'thethinkmill', title: 'Atlassian coordinator' },
+		{ name: '仇紫荷',image: '/images/organiser-gil_davidson.jpg',     twitter: 'iamnotyourbroom',   title: 'Atlassian coordinator' },
+		{ name: '应泽峰', image: '/images/organiser-adam_ahmed.jpg',    twitter: 'hitsthings',   title: 'Atlassian coordinator' },
+		{ name: '傅思喆', image: '/images/organiser-lachlan_hardy.jpg', twitter: 'lachlanhardy', title: 'Atlassian coordinator' }
 	]
 	
 	view.render('site/about');
