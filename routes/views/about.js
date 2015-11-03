f2fbb2ed06b6c1fccc723fe4346a9917b70540ab
zengyan <zengyan03@outlook.com>
2015-11-03 09:29:34 +0800

    feature(main-43):完成

--- routes/views/about.js
+++ routes/views/about.js
@@ -9,11 +9,11 @@ exports = module.exports = function(req, res) {
 	locals.page.title = 'About SydJS';
 	
 	locals.organisers = [
-		{ name: '方糖', image: '/images/organiser-craig_sharkie.jpg', weibo: 'u/5286061009',       title: 'Founder, MC, coordinator' },
-		{ name: '曾䶮', image: '/images/organiser-zeng_yan.jpg',   weibo: 'allenxiaoai', title: 'Atlassian coordinator' },
-		{ name: '仇紫荷',image: '/images/organiser-gil_davidson.jpg',     weibo: 'u/2668961191',   title: 'Atlassian coordinator' },
-		{ name: '应泽峰', image: '/images/organiser-adam_ahmed.jpg',    weibo: 'u/5019398138',   title: 'Atlassian coordinator' },
-		{ name: '傅思喆', image: '/images/organiser-lachlan_hardy.jpg', weibo: 'u/1261158735', title: 'Atlassian coordinator' }
+		{ name: '方糖', image: '/images/organiser-ft.jpg', weibo: 'FangtangStudio', weiboID: 'u/5286061009' ,title: 'Founder, MC, coordinator' },
+		{ name: '曾䶮', image: '/images/organiser-zeng_yan.jpg',   weibo: '曾龑', weiboID: 'allenxiaoai',title: 'Atlassian coordinator' },
+		{ name: '仇紫荷',image: '/images/organiser-qzh.jpg',  weibo: '会铁砂掌的镁', weiboID: 'u/2668961191' ,title: 'Atlassian coordinator' },
+		{ name: '应泽峰', image: '/images/organiser-yzf.jpg',   weibo: 'Yzfff8',  weiboID: 'u/5019398138' ,title: 'Atlassian coordinator' },
+		{ name: '傅思喆', image: '/images/organiser-fsz.jpg', weibo: 'fusizhe', weiboID: 'u/1261158735',title: 'Atlassian coordinator' }
 	]
 	
 	view.render('site/about');
